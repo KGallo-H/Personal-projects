59c0a20d9058222dad4865f182f3c36a218110e5
KGallo-H <77867902+KGallo-H@users.noreply.github.com>
2026-07-18 23:09:53 -0400

    implemented get and put mappings for accountDto

--- banking-app/src/main/java/com/example/banking_app/controller/AccountController.java
+++ banking-app/src/main/java/com/example/banking_app/controller/AccountController.java
@@ -1,7 +1,13 @@
 package com.example.banking_app.controller;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +25,23 @@ public class AccountController {
         this.accountService = accountService;
     }
 
-    public ResponseEntity<AccountDto> createAccount(AccountDto accountDto) {
+    @PostMapping
+    public ResponseEntity<AccountDto> createAccount(@RequestBody AccountDto accountDto) {
         return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
     
+    }
+
+    @GetMapping("/{accountId}")
+    public ResponseEntity<AccountDto> getAccountById(@PathVariable Long accountId) {
+        AccountDto accountDto = accountService.getAccountById(accountId);
+        return ResponseEntity.ok(accountDto);
+    }
+
+    @PutMapping("/{accountId}/deposit")
+    public ResponseEntity<AccountDto> deposit(@PathVariable Long accountId, @RequestBody Map<String, Double> request) {
+        double amount = request.get("amount");
+        AccountDto accountDto = accountService.deposit(accountId, amount);
+        return ResponseEntity.ok(accountDto);
+        
     }
 }
