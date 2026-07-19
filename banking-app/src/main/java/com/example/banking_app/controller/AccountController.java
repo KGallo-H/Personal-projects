581346333846ae49b539a930c4afacfdc2b8ce96
KGallo-H <77867902+KGallo-H@users.noreply.github.com>
2026-07-18 22:07:35 -0400

    create account methods created

---
+++ banking-app/src/main/java/com/example/banking_app/controller/AccountController.java
@@ -0,0 +1,26 @@
+package com.example.banking_app.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.banking_app.dto.AccountDto;
+import com.example.banking_app.service.AccountService;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    private AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    public ResponseEntity<AccountDto> createAccount(AccountDto accountDto) {
+        return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
+    
+    }
+}
--- banking-app/src/main/java/com/example/banking_app/service/impl/AccountServiceImpl.java
+++ banking-app/src/main/java/com/example/banking_app/service/impl/AccountServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.banking_app.service.impl;
 import org.springframework.stereotype.Service;
 
 import com.example.banking_app.dto.AccountDto;
+import com.example.banking_app.entity.Account;
+import com.example.banking_app.mapper.AccountMapper;
 import com.example.banking_app.repository.AccountRepository;
 import com.example.banking_app.service.AccountService;
 
@@ -19,8 +21,9 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public AccountDto createAccount(AccountDto accountDto) {
 
-        return accountDto;
-
+        Account account = AccountMapper.mapToAccount(accountDto);
+        Account savedAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(savedAccount);
     }
 
 }
