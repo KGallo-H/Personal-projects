241b0d95077deef9b9f5bc0a1adcba18176efba4
KGallo-H <77867902+KGallo-H@users.noreply.github.com>
2026-07-18 21:58:53 -0400

    setup initial interface file

--- banking-app/src/main/java/com/example/banking_app/service/AccountService.java
+++ banking-app/src/main/java/com/example/banking_app/service/AccountService.java
@@ -1,7 +1,7 @@
 package com.example.banking_app.service;
 
-import com.example.banking_app.entity.Account;
+import com.example.banking_app.dto.AccountDto;
 
 public interface AccountService {
-    Account createAccount(Account account);
+    AccountDto createAccount(AccountDto accountDto);
 }
